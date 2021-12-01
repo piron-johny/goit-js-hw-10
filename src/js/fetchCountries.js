feada1279d52928be004ac612fe0e4ce16a17296
Piron Evgeniy <85217377+piron-johny@users.noreply.github.com>
2021-12-01 13:11:53 +0200

    5

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 function fetchCountries(name) {
   if (name.length)
     return fetch(
-      `https://restcountries.com/v2/name/${name}?fields=name,capital,population,languages,flag`,
+      `https://restcountries.com/v3/name/${name}?fields=name,capital,population,languages,flag`,
     ).then(response => response.json());
 }
 
